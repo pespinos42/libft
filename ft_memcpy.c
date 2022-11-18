464f23c4cb21d202e4f8c100a00a3af9338a9031
pespinos42 <113940232+pespinos42@users.noreply.github.com>
2022-11-18 14:38:23 +0100

    ft_memcpy ADDED

---
+++ ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pespinos <pespinos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 14:07:58 by pespinos          #+#    #+#             */
+/*   Updated: 2022/10/02 12:21:05 by pespinos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char	*dst2;
+	char	*src2;
+
+	dst2 = (char *) dst;
+	src2 = (char *) src;
+	if (n == 0 || dst2 == src2)
+		return (dst);
+	while (n > 0)
+	{
+		*dst2 = *src2;
+		dst2++;
+		src2++;
+		n--;
+	}
+	return (dst);
+}
