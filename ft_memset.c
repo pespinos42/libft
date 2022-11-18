1d81968ad12bcd0b958da32caf2d451906c9c7ad
pespinos42 <113940232+pespinos42@users.noreply.github.com>
2022-11-18 13:12:25 +0100

    ft_memset ADDED V2

---
+++ ft_memset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pespinos <pespinos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 11:37:24 by pespinos          #+#    #+#             */
+/*   Updated: 2022/10/02 12:31:00 by pespinos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	char	*str;
+	char	letter;
+
+	str = (char *) b;
+	letter = (char) c;
+	while (len > 0)
+	{
+		*str = letter;
+		str++;
+		len--;
+	}
+	return (b);
+}
