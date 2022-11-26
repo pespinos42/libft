8fc6b330f3fc7511bcb025a5e805ba94c7590a89
pespinos42 <113940232+pespinos42@users.noreply.github.com>
2022-11-26 12:15:06 +0100

    ft_lstmap ADDED

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pespinos <pespinos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/01 12:16:09 by pespinos          #+#    #+#             */
+/*   Updated: 2022/10/01 12:37:17 by pespinos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *new;
+	t_list *final = NULL;
+
+	if (!lst || !f)
+		return (NULL);
+	while (lst)
+	{
+		new = ft_lstnew(f(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&final, new);
+		lst = lst->next;
+	}
+	return (final);
+}
